5adcfdbc8bd458321d7473d056f952add1e0fb05
Pat Cichocki <pat.cichocki@gmail.com>
2020-04-01 12:42:52 +0100

    authentication implemented

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 const usersRepo = require('./repositories/users');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieSession({
+  keys: ['sjhgjfdgfgjf5thgf']
+}))
 
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
   res.send(`
+    Your id is: ${req.session.userId}
     <div>
       <form method="POST">
         <input name="email" placeholder="email" />
@@ -20,7 +25,7 @@ app.get('/', (req, res) => {
 
 
 
-app.post('/', async (req, res) => {
+app.post('/signup', async (req, res) => {
   //get access to email, password, passwordConfirmation
   // console.log(req.body);
   const {email, password, passwordConfirmation} = req.body;
@@ -32,8 +37,50 @@ app.post('/', async (req, res) => {
     return res.send('Passwords must match');
   } 
 
+  // create a user in our user repo to represent this person
+  const user = await usersRepo.create({email, password});
+
+  // store the id of that user inside the users cookie
+  req.session.userId = user.id //added by cookie session
+
   res.send('account created');
 });
+
+app.get('/signout', (req, res) => {
+  req.session = null;
+  res.send('you are logout');
+});
+
+app.get('/signin', (req, res) => {
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <button>Sign In</button>
+      </form>
+    </div>
+  `)
+});
+
+app.post('/signin', async (req, res) => {
+  const {email, password} = req.body;
+  const user = await usersRepo.getOneBy({email});
+  if (!user) {
+   return res.send('email not found');
+  }
+  const validPassword = await usersRepo.comparePasswords(
+    user.password,
+    password
+  );
+
+  if(!validPassword) {
+    return res.send('invalid password');
+  }
+
+  req.session.userId = user.id;
+  res.send('you are signed in');
+})
 
 app.listen(3000, () => {
   console.log('listening')
--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const crypto = require('crypto');
+const util = require('util');
+const scrypt = util.promisify(crypto.scrypt);
 
 class UserRepository {
   constructor(filename) {
@@ -20,10 +22,32 @@ class UserRepository {
 
   async create(attrs) {
     attrs.id = this.randomId();
+
+    const salt = crypto.randomBytes(8).toString('hex');
+    const buf = await scrypt(attrs.password, salt, 64);
+
     const records = await this.getAll();
-    records.push(attrs);
-    //write the updated 'records' array back to this.filename
+    const record = ({
+      ...attrs,
+      password: `${buf.toString('hex')}.${salt}`
+    })
+    records.push(record);
+    
     await this.writeAll(records);
+    return record;
+
+  }
+  
+  async comparePasswords(saved, supplied) {
+    //saved --> password saved in our db hashed.salt
+    //supplied --> password given by the user
+    // const[hashed, salt] = saved.split('.')
+    const result = saved.split('.');
+    const hashed = result[0];
+    const salt = result[1];
+
+    const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
+    return hashed === hashedSuppliedBuf.toString('hex');
 
   }
 
