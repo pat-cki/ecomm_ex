2a15b682b7c52d2f3e7415a70d11689c0a33ce21
Pat Cichocki <pat.cichocki@gmail.com>
2020-04-02 12:18:09 +0100

    input errors implementation

--- repositories/users.js
+++ repositories/users.js
@@ -1,23 +1,29 @@
 const fs = require('fs');
 const crypto = require('crypto');
 const util = require('util');
+
 const scrypt = util.promisify(crypto.scrypt);
 
-class UserRepository {
+class UsersRepository {
   constructor(filename) {
-    if(!filename){
-      throw new Error('Creating a new repository requires a filename');
+    if (!filename) {
+      throw new Error('Creating a repository requires a filename');
     }
+
     this.filename = filename;
-    //check if file exists
     try {
       fs.accessSync(this.filename);
-    } catch(err) {
+    } catch (err) {
       fs.writeFileSync(this.filename, '[]');
     }
   }
+
   async getAll() {
-    return JSON.parse(await fs.promises.readFile(this.filename, {encoding: 'utf8'}));
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: 'utf8'
+      })
+    );
   }
 
   async create(attrs) {
@@ -27,33 +33,31 @@ class UserRepository {
     const buf = await scrypt(attrs.password, salt, 64);
 
     const records = await this.getAll();
-    const record = ({
+    const record = {
       ...attrs,
       password: `${buf.toString('hex')}.${salt}`
-    })
+    };
     records.push(record);
-    
+
     await this.writeAll(records);
+
     return record;
-
   }
-  
+
   async comparePasswords(saved, supplied) {
-    //saved --> password saved in our db hashed.salt
-    //supplied --> password given by the user
-    // const[hashed, salt] = saved.split('.')
-    const result = saved.split('.');
-    const hashed = result[0];
-    const salt = result[1];
-
+    // Saved -> password saved in our database. 'hashed.salt'
+    // Supplied -> password given to us by a user trying sign in
+    const [hashed, salt] = saved.split('.');
     const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
-    return hashed === hashedSuppliedBuf.toString('hex');
 
+    return hashed === hashedSuppliedBuf.toString('hex');
   }
 
-  async writeAll(records){
-    await fs.promises.writeFile(this.filename,JSON.stringify(records, null, 2));
-
+  async writeAll(records) {
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(records, null, 2)
+    );
   }
 
   randomId() {
@@ -74,46 +78,33 @@ class UserRepository {
   async update(id, attrs) {
     const records = await this.getAll();
     const record = records.find(record => record.id === id);
+
     if (!record) {
       throw new Error(`Record with id ${id} not found`);
     }
+
     Object.assign(record, attrs);
     await this.writeAll(records);
-
   }
 
   async getOneBy(filters) {
     const records = await this.getAll();
 
-    for(let record of records) {
+    for (let record of records) {
       let found = true;
+
       for (let key in filters) {
-        if(record[key] !== filters[key]){
+        if (record[key] !== filters[key]) {
           found = false;
         }
       }
-      if (found === true) {
-        return record; 
-      }
 
+      if (found) {
+        return record;
+      }
     }
   }
-
-
 }
 
-// const test = async () => {
-
-//   const repo = new UserRepository('users.json');
-//   // const user = await repo.getOne('42833b003')
-//   // await repo.create({email: 'test@test.com', password: 'password'});
-//   // const users = await repo.getAll();
-//   // console.log(user);
-//   // await repo.create({email: 'test@test.com' });
-//   const user = await repo.getOneBy({email: 'test@test.com'});
-//   console.log(user)
-// }
-
-// test(); 
-
-module.exports = new UserRepository('users.json');
+module.exports = new UsersRepository('users.json');
+ 
--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
+
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
-const {requireEmail, requirePassword, requirePasswordConfirmation} = require('./validators');
+const {
+  requireEmail,
+  requirePassword,
+  requirePasswordConfirmation,
+  requireEmailExists,
+  requireValidPasswordForUser
+} = require('./validators');
 
 const router = express.Router();
 
@@ -12,50 +19,48 @@ router.get('/signup', (req, res) => {
 });
 
 router.post(
-    '/signup',
-    [requireEmail, requirePassword, requirePasswordConfirmation], 
-    async (req, res) => {
-      const errors = validationResult(req);
-      console.log(errors); 
-      if (!errors.isEmpty()){
-        return res.send(signupTemplate({req, errors}));
-      } 
-      const { email, password, passwordConfirmation } = req.body;
-  
-  
-      const user = await usersRepo.create({email, password});
+  '/signup',
+  [requireEmail, requirePassword, requirePasswordConfirmation],
+  async (req, res) => {
+    const errors = validationResult(req);
 
-      req.session.userId = user.id;
+    if (!errors.isEmpty()) {
+      return res.send(signupTemplate({ req, errors }));
+    }
 
-      res.send('account created');
-});
+    const { email, password, passwordConfirmation } = req.body;
+    const user = await usersRepo.create({ email, password });
+
+    req.session.userId = user.id;
+
+    res.send('Account created!!!');
+  }
+);
 
 router.get('/signout', (req, res) => {
   req.session = null;
-  res.send('you are logout');
+  res.send('You are logged out');
 });
 
 router.get('/signin', (req, res) => {
-  res.send(signinTemplate())
+  res.send(signinTemplate());
 });
 
-router.post('/signin', async (req, res) => {
-  const {email, password} = req.body;
-  const user = await usersRepo.getOneBy({email});
-  if (!user) {
-   return res.send('email not found');
-  }
-  const validPassword = await usersRepo.comparePasswords(
-    user.password,
-    password
-  );
+router.post(
+  '/signin',
+  [requireEmailExists, requireValidPasswordForUser],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
 
-  if(!validPassword) {
-    return res.send('invalid password');
-  }
+    const { email } = req.body;
 
-  req.session.userId = user.id;
-  res.send('you are signed in');
-})
+    const user = await usersRepo.getOneBy({ email });
+
+    req.session.userId = user.id;
+
+    res.send('You are signed in!!!');
+  }
+);
 
 module.exports = router;
